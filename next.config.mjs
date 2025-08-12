d59c7f76c50eed54861b562bc7d21407aa836a26
zeebooksalot <ryan@ryanzee.com>
2025-08-12 16:33:41 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: false,
+    ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: false,
